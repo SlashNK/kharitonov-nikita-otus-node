45a94c8a4d1afa2688f3b1cecfc3ac6ef2901b2b
Nikita Kharitonov <n.kharitonov@econom.gazprom.ru>
2024-06-24 19:15:07 +0300

    docs: swagger

--- homework/node-4/routes/v1/index.js
+++ homework/node-4/routes/v1/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const { usersApiRouter } = require("./users");
 const { exercisesApiRouter } = require("./exercises");
 const { workoutBlocksApiRouter } = require("./workout-blocks");
@@ -12,5 +14,6 @@ router.use("/exercises", exercisesApiRouter);
 router.use("/workout-blocks", workoutBlocksApiRouter);
 router.use("/workout-templates", workoutTemplatesApiRouter);
 router.use("/workout-sessions", workoutSessionsApiRouter);
-
+router.use("/docs", swaggerUi.serve);
+router.get("/docs", swaggerUi.setup(swaggerDocument));
 module.exports = router;
